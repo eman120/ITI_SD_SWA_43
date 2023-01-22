64d6165499efb5ade6de8979f2a833dfeab941a5
Eman Mohammed <76956637+eman120@users.noreply.github.com>
2023-01-22 23:52:33 +0200

    Add files via upload

---
+++ C#/D03 task/ConsoleApp1/Class1.cs
@@ -0,0 +1,153 @@
+﻿namespace ConsoleApp1
+{
+    public enum Gender
+    {
+        M , F
+    }
+
+    [Flags]
+    public enum privilege:byte
+    {
+        //CRUD
+        //1000 => 8 
+        //0100 => 4
+        //0010 => 2
+        //0001 => 0
+        guest = 0x04, //R
+        Developer = 0x0e, //CRU
+        secretary = 0x0c, // CR
+        DBA = 0x0d, // CRD
+        officer = 0x0f //CRUD
+    }
+
+    public struct HireDate
+    {
+
+        int day;
+        int month;
+        int year;
+        public HireDate(int day, int month, int year)
+        {
+            this.day = day;
+            this.month = month;
+            this.year = year;
+        }
+        //public int Day{
+        //    set{ day = value; }
+        //    get{ return day; }
+        //}
+        public void setDay(int _day)
+        {
+            this.day = _day;
+        } 
+
+        public int getDay()
+        {
+            return this.day;
+        }
+
+        public void setMonth(int _month)
+        {
+            this.month = _month;
+        }
+        public int getMonth()
+        {
+            return this.month;
+        }
+
+        public void setYear(int _year)
+        {
+            this.year = _year;
+        }
+        public int getYear()
+        {
+            return this.year;
+        }
+
+        public override string ToString()
+        {
+            return $"HireDate is : {this.day}/{this.month}/{this.year}";
+        }
+    }
+    public struct Employee
+    {
+        int ID;
+        privilege secutityLevel;
+        int salary;
+        HireDate date;
+        Gender gender;
+        public Employee(int ID , privilege secutity_level , int salary , HireDate date , Gender gender)
+        {
+            this.ID = ID;
+            this.secutityLevel = secutity_level;
+            this.salary = salary;
+            this.date = date;   
+            this.gender = gender;
+        }
+
+        public int getID()
+        {
+            return this.ID;
+        }
+
+        public void setID (int ID)
+        {
+            this.ID = ID;
+        }
+        public privilege getSecutityLevel() {
+            return this.secutityLevel;
+        }
+        public void setSecutityLevel(privilege secutityLevel)
+        {
+            this.secutityLevel = secutityLevel;
+        }
+
+        public int getSalary()
+        {
+            return this.salary;
+        }
+
+        public void setSalary(int salary)
+        {
+            this.salary = salary;
+        }
+
+        public string getDate()
+        {
+            return $"{this.date.getDay()} / {this.date.getMonth()} / {this.date.getYear()}";
+        }
+
+        public void setDate(int day, int month, int year)
+        {
+            this.date.setDay(day);
+            this.date.setMonth(month);
+            this.date.setYear(year);
+        }
+
+        public Gender getGender()
+        {
+            return this.gender;
+        }
+
+        public void setGender(Gender gender)
+        {
+            this.gender = gender;
+        }
+
+        public override string ToString()
+        {
+            return $"""
+                Emp ID : {this.ID}
+                Secutity Level : {this.secutityLevel}
+                Salary : {this.salary:C}
+                Gender : {this.gender}
+                {this.date.ToString()}
+                """
+                
+                ;
+        }
+    }
+
+
+
+}
---
+++ C#/D03 task/ConsoleApp1/Program.cs
@@ -0,0 +1,91 @@
+﻿using System.Reflection;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sl = String.Empty;
+            string gender = String.Empty;
+            string date = String.Empty;
+
+            Console.WriteLine("Please enter the number of employees");
+            Console.WriteLine("================================");
+
+            int size = int.Parse(Console.ReadLine());
+            Console.WriteLine("================================");
+
+            Employee[] empArr = new Employee[size];
+
+            for (int i = 0; i < empArr.Length; i++)
+            {
+                Console.WriteLine("Please enter the data of employee " + (i + 1));
+                Console.WriteLine("================================");
+                //ID
+                Console.WriteLine("Enter Employee ID : ");
+                empArr[i].setID(int.Parse(Console.ReadLine()));
+
+                //security Level
+                Console.WriteLine("Enter Employee Secutity Level (guest/Developer/secretary/DBA/officer) : ");
+                sl=Console.ReadLine();
+                empArr[i].setSecutityLevel((privilege)Enum.Parse(typeof(privilege),sl));
+
+                //salary
+                Console.WriteLine("Enter Employee salary : ");
+                empArr[i].setSalary(int.Parse(Console.ReadLine()));
+
+                //gender
+                do
+                {
+                    Console.WriteLine("Enter Employee gender (M / F) only : ");
+                    gender = Console.ReadLine();
+                } while (gender != "F" && gender != "M");
+                empArr[i].setGender((Gender)Enum.Parse(typeof(Gender), gender));
+
+                //hire date
+                Console.WriteLine("Enter Employee hire date (dd/mm/yyyy) : ");
+                date = Console.ReadLine();
+                string[] dateArr = date.Split("/");
+                empArr[i].setDate(int.Parse(dateArr[0]) , int.Parse(dateArr[1]) , int.Parse(dateArr[2]));
+
+
+                Console.WriteLine("================================");
+                //empArr[i] = new Employee();
+            }
+
+            Console.WriteLine("Press D to display all employees");
+            if (Console.ReadKey().Key == ConsoleKey.D)
+            {
+                Console.Clear();
+                displayEmployees(empArr);
+            }
+
+            ////All Employees
+            //for (int i = 0; i < empArr.Length; i++)
+            //{
+            //    Console.Clear();
+            //    Console.WriteLine(empArr[i].ToString());
+            //    Console.WriteLine("================================");
+            //}
+
+
+        }
+        public static void displayEmployees(Employee[] empArr)
+        {
+            //All Employees
+            for (int i = 0; i < empArr.Length; i++)
+            {
+                Console.WriteLine(empArr[i].ToString());
+                Console.WriteLine("================================");
+            }
+        }
+
+        //public static void Hold()
+        //{
+        //    Console.WriteLine("Press d to return");
+        //    while (Console.ReadKey().Key != ConsoleKey.D) ;
+        //}
+
+    }
+}
