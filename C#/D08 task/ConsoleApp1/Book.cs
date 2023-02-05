8c35bb6118ede370d14a5d5f29a4080eaacddbf5
Eman Mohammed <76956637+eman120@users.noreply.github.com>
2023-02-05 11:57:51 +0200

    upload day 8

---
+++ C#/D08 task/ConsoleApp1/Book.cs
@@ -0,0 +1,83 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public delegate string DemoDelDT(Book book);
+    public delegate string Predicate<T>(T book);
+
+
+    public class Book
+    {
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string[] Authors { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public decimal Price { get; set; }
+        public Book(string _ISBN, string _Title,
+        string[] _Authors, DateTime _PublicationDate,
+        decimal _Price)
+        {
+            ISBN = _ISBN;
+            Title = _Title; 
+            Authors = _Authors;
+            PublicationDate = _PublicationDate;
+            Price = _Price;
+        }
+        public override string ToString()
+        {
+            return
+                $"ISBN ={ISBN}" +
+                 $"Title = {Title} " +
+                 $"Authors = {Authors}" +
+                 $"PublicationDate = {PublicationDate} " +
+                 $"Price = {Price}";
+        }
+    }
+    public class BookFunctions
+    {
+        public static string GetTitle(Book B)
+        {
+            return B?.Title;
+        }
+        public static string GetAuthors(Book B)
+        {
+            string author = string.Empty;
+            for(int i = 0; i< B?.Authors.Length; i++)
+            {
+                author += B?.Authors?[i];
+                if(i < B?.Authors.Length-1)
+                    author += ",";
+            }
+            return author;
+        }
+        public static string GetPrice(Book B)
+        {
+            return $"price = {B?.Price}";
+        }
+    }
+
+    public class LibraryEngine
+    {
+        public static void ProcessBooks(List<Book> bList
+        ,/*Pointer To BookFunciton*/ Predicate<Book> fPtr)
+        {
+            foreach (Book B in bList)
+            {
+                Console.WriteLine(fPtr(B));
+            }
+        }
+        public static void ProcessBooks(List<Book> bList
+        ,/*Pointer To BookFunciton*/ DemoDelDT fPtr)
+        {
+            foreach (Book B in bList)
+            {
+                Console.WriteLine(fPtr(B));
+            }
+        }
+    }
+}
---
+++ C#/D08 task/ConsoleApp1/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string isbn = "b1";
+            string title = "life";
+            string[] authors = {"Eman" , "Mohammed" };
+            DateTime time = DateTime.Now;
+            decimal price = 12;
+
+            string R = string.Empty;
+            Book book1 = new Book(isbn, title, authors, time, price);
+            Book book2 = new Book(isbn, title, authors, time, price);
+
+            List<Book> bList = new List<Book>();
+            bList.Add(book1);
+            //bList.Add(book2);
+
+            #region a. User Defined Delegate Datatype
+            Console.WriteLine("a. User Defined Delegate Datatype");
+            DemoDelDT fptr;
+            fptr = new DemoDelDT(BookFunctions.GetTitle);
+            LibraryEngine.ProcessBooks(bList, fptr);
+
+            fptr = new DemoDelDT(BookFunctions.GetPrice);
+            LibraryEngine.ProcessBooks(bList, fptr);
+
+            fptr = new DemoDelDT(BookFunctions.GetAuthors);
+            LibraryEngine.ProcessBooks(bList, fptr);
+            #endregion
+
+            Console.WriteLine("==================================");
+            #region b. BCL Delegates
+            Console.WriteLine("b. BCL Delegates");
+            Predicate<Book> fptr2;
+            fptr2 = new Predicate<Book>(BookFunctions.GetTitle);
+            LibraryEngine.ProcessBooks(bList, fptr2);
+
+            fptr2 = new Predicate<Book>(BookFunctions.GetPrice);
+            LibraryEngine.ProcessBooks(bList, fptr2);
+
+            fptr2 = new Predicate<Book>(BookFunctions.GetAuthors);
+            LibraryEngine.ProcessBooks(bList, fptr2);
+            #endregion
+
+            Console.WriteLine("==================================");
+            #region c.Anonymous Method(GetISBN)
+            Console.WriteLine("c.Anonymous Method(GetISBN)");
+            Predicate<Book> fPtr3;
+            //new Predicate<Book>(BookFunctions.GetTitle);
+            fPtr3 = delegate (Book b) { return b.ISBN; };
+            LibraryEngine.ProcessBooks(bList, fPtr3);
+            #endregion
+
+            Console.WriteLine("==================================");
+            #region d.Lambda Expression(GetPublicationDate)
+            Console.WriteLine("d.Lambda Expression(GetPublicationDate)");
+            Predicate <Book> fPtr4 ;
+            //new Predicate<Book>(BookFunctions.GetTitle)
+            fPtr4 =  (Book b) => { return $"{b.PublicationDate}"; };
+            LibraryEngine.ProcessBooks(bList, fPtr4);
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+            //R = fptr?.Invoke(book) ?? "invalid";
+
+            //Console.WriteLine("GetTitle " + R);
+
+            //R = fptr?.Invoke(book) ?? "invalid";
+
+            //Console.WriteLine("GetPrice " + R);
+
+            //R = fptr?.Invoke(book) ?? "invalid";
+            //string[] Arr = R.Split(',');
+            //for(int i = 0 ; i < authors.Length; i++) 
+            //{           
+            //    Console.WriteLine($"GetAuthor {i+1}) : {Arr[i]}");
+            //}
+        }
+    }
+}
