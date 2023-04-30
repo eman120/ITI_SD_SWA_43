2f0eacfdb1a2b2dc6e29ec487f76b18454841e27
Eman Mohammed <76956637+eman120@users.noreply.github.com>
2023-04-30 15:17:15 +0300

    upload MVVM Day1 task

---
+++ MVVM/Day1/D01 task/ITI.WPFCarModel.MVVM/ITI.WPFCarModel.MVVM/Model/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.WPFCarModel.MVVM.Model
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public String Name { get; set; }
+        public String Color { get; set; }
+        public String Model { get; set; }
+    }
+}
---
+++ MVVM/Day1/D01 task/ITI.WPFCarModel.MVVM/ITI.WPFCarModel.MVVM/ViewModel/FirstViewModel.cs
@@ -0,0 +1,55 @@
+﻿using ITI.WPFCarModel.MVVM.Commands;
+using ITI.WPFCarModel.MVVM.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ITI.WPFCarModel.MVVM.ViewModel
+{
+    public class FirstViewModel
+    {
+        public ObservableCollection<Car> CarList { get; set; }
+        public ICommand AddCommand { get; set; }
+        public Car NewCar { get; set; }
+
+        public FirstViewModel()
+        {
+            AddCommand = new AddCarCommand(Add, CanAdd);
+
+            NewCar = new();
+            CarList = new ObservableCollection<Car>()
+            {
+                new Car { Name = "Toyota Camry", Id = 1, Price = 25000, Model = "2022" , Color = "Red"},
+                new Car { Name = "Honda Accord", Id = 2, Price = 28000, Model = "2022" , Color = "LightBlue"},
+                new Car { Name = "Ford Mustang", Id = 3, Price = 40000, Model = "2022" , Color = "LightPink"},
+                new Car { Name = "Chevrolet Corvette", Id = 4, Price = 70000, Model = "2022" , Color = "Yellow"}
+                //new Car { Name = "Tesla Model S", Id = 5, Price = 90000, Model = "2022", Color = "White" }
+        };
+
+        }
+
+        private bool CanAdd(object obj)
+        {
+            return true;
+        }
+
+        private void Add(object obj)
+        {
+            Car NCar = obj as Car;
+            var car = new Car();
+            car.Color = NCar.Color;
+            car.Name = NCar.Name;
+            car.Model = NCar.Model;
+            car.Price = NCar.Price;
+            car.Id = NCar.Id;
+            CarList.Add(car);
+            MessageBox.Show("Added");
+
+        }
+    }
+}
